8cb1bead87b981cf6e6f7ee97fff8f19a4980144
SvetaKrivalova <svetakrivalova123@gmail.com>
2024-11-11 18:45:11 +0700

    Изменение экрана загрузки

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,43 @@
 
-
 function openLoadingScreen() {
  var LoadingScreen = document.querySelector('.LoadingScreen');
+ document.querySelector('.overlay').classList.add('show');
+ document.querySelector('.LoadingScreen').classList.add('show');
  LoadingScreen.style.display = 'block';
 }
 
- function closeLoadingScreen() {
+function closeLoadingScreen() {
     var LoadingScreen = document.querySelector('.LoadingScreen');
+    document.querySelector('.overlay').classList.remove('show');
+    document.querySelector('.LoadingScreen').classList.remove('show');
     LoadingScreen.style.display = 'none';
-}
 
+   
+}
 
 document.getElementById('showUploadForm').addEventListener('click', function() {
     const uploadForm = document.getElementById('uploadForm');
     uploadForm.style.display = uploadForm.style.display === 'none' ? 'block' : 'none';
-});
+})
 
+function updateFileLabel(files) {
+       const fileLabel = document.getElementById('file-label');
+       const fileNames = Array.from(files).map(file => file.name).join(', ');
+       fileLabel.textContent = fileNames;
+}
+
+function zoomImage(src, shelfId) {
+       const zoomedImage = document.getElementById('zoomedImage');
+       const zoomedImg = document.getElementById('zoomedImg');
+       const shelfInfo = document.getElementById('shelfInfo');
+       const deleteButton = document.getElementById('deleteButton');
+       zoomedImg.src = src;
+       shelfInfo.innerText = `Номер полки: ${shelfId}`;
+       deleteButton.setAttribute('onclick', `deleteImage(event, '${shelfId}')`);
+       zoomedImage.style.display = 'flex';
+}
+
+function closeZoom() {
+       const zoomedImage = document.getElementById('zoomedImage');
+       zoomedImage.style.display = 'none';
+}
