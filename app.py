604dbe45570eade17bd1888639a255fe704911a2
Mialenna <annakupriyanova04@mail.ru>
2024-11-26 20:46:35 +0700

    моделька прикреплена

--- app.py
+++ app.py
@@ -4,16 +4,21 @@ import shutil
 import cv2
 import mimetypes
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from ultralytics import YOLO
+import random
+from sqlalchemy import func, text
+from sklearn.model_selection import train_test_split
 
 load_dotenv()
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+secret_key = os.urandom(24)
+app.secret_key = secret_key
 
 db = SQLAlchemy(app)
 
@@ -23,6 +28,17 @@ class Tree(db.Model):
     processed_photo = db.Column(db.String(255), nullable=True)
     class_name = db.Column(db.Enum('Disease', 'Health'), nullable=False, default='Health')
     photo_date = db.Column(db.DateTime, nullable=False)
+
+class Yolka(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    photo = db.Column(db.String(255), nullable=True)
+    txt = db.Column(db.String(255), nullable=True)
+    photo_date = db.Column(db.DateTime, nullable=True)
+
+class Datasets(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    dataset_name = db.Column(db.String(255), nullable=False)
+    dataset_path = db.Column(db.String(255), nullable=False)
 
 with app.app_context():
     db.create_all()
@@ -187,6 +203,326 @@ def run_yolo_predictions():
 
     if os.path.exists(run):
         shutil.rmtree(run)
+
+
+
+
+
+@app.route('/model')
+def model():
+    yolki = Yolka.query.order_by(Yolka.id.desc()).all()
+    filenames = [os.path.basename(yolka.photo) for yolka in yolki]
+    non_empty_count = db.session.query(func.count(Yolka.txt)).filter(Yolka.txt.isnot(None), Yolka.txt != '').scalar()
+
+    datasets = Datasets.query.all()
+
+    return render_template('model.html', yolki=yolki, filenames=filenames, non_empty_count=non_empty_count, datasets=datasets)
+
+@app.route('/upload_model_files', methods=['POST'])
+def upload_model_files():
+    if 'files' not in request.files:
+        return "No file part", 400
+    
+    files = request.files.getlist('files')
+    images_dir = os.path.join(os.path.dirname(__file__), "static", "model_images")
+    if not os.path.exists(images_dir):
+        os.makedirs(images_dir)
+
+    new_entries = []
+    for file in files:
+        if file.filename == '':
+            return "No selected file", 400
+        
+        filename = file.filename
+        file_extension = os.path.splitext(filename)[1].lower()
+        file_path = os.path.join(images_dir, filename)
+        
+        try:
+            file.save(file_path)
+            print(f"Файл сохранен: {file_path}")
+        except Exception as e:
+            print(f"Ошибка при сохранении файла {filename}: {e}")
+            return "Error saving file", 500
+
+    for file in files:
+        filename = file.filename
+        file_extension = os.path.splitext(filename)[1].lower()
+        relative_image_path = os.path.relpath(os.path.join(images_dir, filename), start=os.path.join(os.path.dirname(__file__), "static"))
+        relative_image_path = relative_image_path.replace("\\", "/")
+        
+        txt_path = None
+        
+        if file_extension != '.txt':
+            txt_filename = os.path.splitext(filename)[0] + '.txt'
+            txt_file_path = os.path.join(images_dir, txt_filename)
+            
+            if os.path.exists(txt_file_path):
+                txt_path = os.path.relpath(txt_file_path, start=os.path.join(os.path.dirname(__file__), "static"))
+                txt_path = txt_path.replace("\\", "/")
+                print(f"Текстовый файл найден: {txt_path}")
+            else:
+                print(f"Текстовый файл не найден: {txt_file_path}")
+
+            new_entries.append(Yolka(photo=relative_image_path, txt=txt_path, photo_date=datetime.now()))
+
+    try:
+        db.session.bulk_save_objects(new_entries)
+        db.session.commit()
+        print("Записи успешно добавлены в базу данных.")
+    except Exception as e:
+        db.session.rollback()
+        print(f"Ошибка при сохранении в базу данных: {e}")
+        return "Error saving to database", 500
+
+    return redirect(url_for('model')) 
+
+@app.route('/copy_photos', methods=['POST'])
+def copy_photos():
+    num_photos = request.form.get('num_photos', type=int)
+    dataset_name = request.form.get('dataset_name') 
+    destination_folder = request.form.get('destination_folder') 
+    train_size = request.form.get('train_size', type=float)
+    val_size = request.form.get('val_size', type=float)
+
+    if dataset_name is None:
+        return jsonify({"error": "Необходимо указать название датасета."}), 400
+
+    if destination_folder is None:
+        return jsonify({"error": "Необходимо указать путь к папке назначения."}), 400
+
+    if train_size + val_size != 1.0:
+        return jsonify({"error": "Сумма train_size и val_size должна быть равна 1."}), 400
+
+    new_dataset = Datasets(dataset_name=dataset_name)
+
+    yolo_folder = os.path.join(destination_folder, dataset_name) 
+    dataset_folder = os.path.join(yolo_folder, 'dataset')
+
+    if os.path.exists(yolo_folder):
+        return jsonify({"exists": True, "message": "Папка с таким именем уже существует. Продолжить?"}), 409
+
+    try:
+        os.makedirs(yolo_folder, exist_ok=True)
+        os.makedirs(dataset_folder, exist_ok=True)
+        os.makedirs(os.path.join(yolo_folder, 'train'), exist_ok=True)
+        os.makedirs(os.path.join(yolo_folder, 'val'), exist_ok=True)
+    except Exception as e:
+        return jsonify({"error": f"Не удалось создать папку: {str(e)}"}), 500
+
+    photos = Yolka.query.filter(Yolka.txt.isnot(None)).all()
+    print(f"Количество фотографий с текстом в базе данных: {len(photos)}")
+    
+    if not photos:
+        return jsonify({"error": "Нет фотографий с текстом в базе данных."}), 400
+
+    if num_photos > len(photos):
+        return jsonify({"error": "Недостаточно фотографий с текстом в базе данных."}), 400
+
+    selected_photos = random.sample(photos, num_photos)
+
+    for photo in selected_photos:
+        photo_path = os.path.join('static', photo.photo)
+        print(f"Исходный путь к изображению: {photo_path}")
+        
+        destination_photo_path = os.path.join(dataset_folder, os.path.basename(photo.photo))  # Сохраняем в dataset
+        print(f"Путь к копируемому изображению: {destination_photo_path}")
+        
+        try:
+            if os.path.exists(photo_path):
+                shutil.copy(photo_path, destination_photo_path)
+            else:
+                print(f"Файл изображения не найден: {photo_path}")
+                return jsonify({"error": f"Файл изображения не найден: {photo_path}"}), 404
+        except Exception as e:
+            print(f"Ошибка при копировании файла {photo.photo}: {str(e)}")
+            return jsonify({"error": f"Ошибка при копировании файла {photo.photo}: {str(e)}"}), 500
+
+        if photo.txt:
+            txt_path = os.path.join('static', photo.txt)
+            print(f"Исходный путь к текстовому файлу: {txt_path}")
+            
+            destination_txt_path = os.path.join(dataset_folder, os.path.basename(photo.txt))  # Сохраняем в dataset
+            print(f"Путь к копируемому текстовому файлу: {destination_txt_path}")
+            
+            try:
+                if os.path.exists(txt_path):
+                    shutil.copy(txt_path, destination_txt_path)
+                else:
+                    print(f"Текстовый файл не найден: {txt_path}")
+                    return jsonify({"error": f"Текстовый файл не найден: {txt_path}"}), 404
+            except Exception as e:
+                print(f"Ошибка при копировании текстового файла {photo.txt}: {str(e)}")
+                return jsonify({"error": f"Ошибка при копировании текстового файла {photo.txt}: {str(e)}"}), 500
+
+    split_and_save_dataset(dataset_folder, yolo_folder, test_size=val_size)
+    
+    new_dataset.dataset_path = yolo_folder
+    try:
+        db.session.add(new_dataset)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback() 
+        return jsonify({"error": f"Не удалось сохранить датасет в базе данных: {str(e)}"}), 500
+
+    return redirect(url_for('model')) 
+
+def split_and_save_dataset(source_folder, destination_folder, test_size):
+    all_files = os.listdir(source_folder)
+    
+    images = [f for f in all_files if f.endswith(('.jpg', '.jpeg', '.png'))]
+    texts = [f for f in all_files if f.endswith('.txt')]
+
+    images_with_texts = [img for img in images if os.path.splitext(img)[0] + '.txt' in texts]
+    train_files, val_files = train_test_split(images_with_texts, test_size=test_size, random_state=42)
+
+    train_folder = os.path.join(destination_folder, 'train')
+    val_folder = os.path.join(destination_folder, 'val')
+
+    os.makedirs(train_folder, exist_ok=True)
+    os.makedirs(val_folder, exist_ok=True)
+
+    for file in train_files:
+        shutil.copy(os.path.join(source_folder, file), os.path.join(train_folder, file))
+        txt_file = os.path.splitext(file)[0] + '.txt'
+        if txt_file in texts:
+            shutil.copy(os.path.join(source_folder, txt_file), os.path.join(train_folder, txt_file))
+
+    for file in val_files:
+        shutil.copy(os.path.join(source_folder, file), os.path.join(val_folder, file))
+        txt_file = os.path.splitext(file)[0] + '.txt'
+        if txt_file in texts:
+            shutil.copy(os.path.join(source_folder, txt_file), os.path.join(val_folder, txt_file))
+
+    print(f"Количество файлов в train: {len(train_files)}")
+    print(f"Количество файлов в val: {len(val_files)}")
+
+@app.route('/create_class', methods=['POST'])
+def create_class():
+    selected_dataset = request.form.get('selected_dataset')
+    class_name = request.form.get('class_name')
+
+    print(f"Выбранный датасет: {selected_dataset}")
+    print(f"Имя класса: {class_name}") 
+
+    if selected_dataset and class_name:
+        dataset_folder = selected_dataset 
+        print(f"Папка датасета: {dataset_folder}")
+
+        classes_file_path = os.path.join(dataset_folder, 'classes.txt')
+        data_yaml_path = os.path.join(dataset_folder, 'data.yaml')
+
+        try:
+            with open(classes_file_path, 'a') as f:
+                f.write(class_name + '\n')
+            print(f"Имя класса '{class_name}' добавлено в файл '{classes_file_path}'")
+        except Exception as e:
+            print(f"Ошибка при записи в {classes_file_path}: {e}")
+
+        try:
+            with open(classes_file_path, 'r') as f:
+                class_names = [line.strip() for line in f.readlines()]
+            print(f"Имена классов: {class_names}")
+        except Exception as e:
+            print(f"Ошибка при чтении из {classes_file_path}: {e}")
+            class_names = []
+
+        num_classes = len(class_names)
+
+        try:
+            with open(data_yaml_path, 'w') as f:
+                f.write(f"train: ./train\n")
+                f.write(f"val: ./val\n")
+                f.write(f"nc: {num_classes}\n")
+                f.write(f"names: {class_names}\n")
+            print(f"Файл {data_yaml_path} успешно создан.")
+        except Exception as e:
+            print(f"Ошибка при записи в {data_yaml_path}: {e}")
+    else:
+        print("Не удалось получить выбранный датасет или имя класса.")
+    
+    return redirect(url_for('model')) 
+
+@app.route('/create_train_script', methods=['POST'])
+def create_train_script():
+    try:
+        imgsz = int(request.form['imgsz'])
+        epochs = int(request.form['epochs'])
+        batch = int(request.form['batch'])
+        save_period = int(request.form['save_period'])
+        selected_dataset = request.form['selected_dataset']
+
+        if not selected_dataset.endswith(os.path.sep):
+            selected_dataset += os.path.sep
+
+        script_path = os.path.join(selected_dataset, 'train.py')
+        
+        path = selected_dataset.replace("\\", "\\\\")
+        
+        script_content = f"""import torch
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+from ultralytics import YOLO
+
+if __name__ == "__main__":
+    model = YOLO('yolo11n.pt')
+    results = model.train(
+        data='{os.path.join(path, 'data.yaml')}',
+        imgsz={imgsz},
+        epochs={epochs},
+        batch={batch},
+        save_period={save_period}
+    )
+"""
+
+        with open(script_path, 'w') as f:
+            f.write(script_content)
+
+        print('Файл train.py успешно создан!', 'success')
+    except Exception as e:
+        print(f'Ошибка при создании файла: {e}', 'error')
+
+    return redirect(url_for('model'))
+
+@app.route('/train', methods=['POST'])
+def train():
+    os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+    try:
+        imgsz = int(request.form['imgsz'])
+        epochs = int(request.form['epochs'])
+        batch = int(request.form['batch'])
+        save_period = int(request.form['save_period'])
+        selected_dataset = request.form['selected_dataset']
+
+        if not selected_dataset.endswith(os.path.sep):
+            selected_dataset += os.path.sep
+
+        path = os.path.normpath(selected_dataset)
+        data_yaml_path = os.path.join(path, 'data.yaml')
+
+        if not os.path.exists(data_yaml_path):
+            print(f'Ошибка: файл не найден по пути {data_yaml_path}')
+            return redirect(url_for('model'))
+        
+        results_dir = os.path.join(path, 'runs')
+        os.makedirs(results_dir, exist_ok=True)
+
+        model = YOLO('yolo11n.pt')
+        results = model.train(
+            data=data_yaml_path,
+            imgsz=imgsz,
+            epochs=epochs,
+            batch=batch,
+            save_period=save_period,
+            name=results_dir 
+        )
+        print('Обучение завершено успешно!', 'success')
+    except Exception as e:
+        print(f'Ошибка при обучении: {e}', 'error')
+    
+    return redirect(url_for('model'))
+
+
 
 if __name__ == '__main__':
     with app.app_context():
--- static/js/script.js
+++ static/js/script.js
@@ -61,3 +61,200 @@ function closeZoom() {
     const zoomedImage = document.getElementById('zoomedImage');
     zoomedImage.style.display = 'none';
 }
+
+
+
+
+
+function updateImage() {
+    var select = document.getElementById("fruits");
+    var selectedFileName = select.options[select.selectedIndex].text;
+    var selectedFilePath = select.value;
+    document.getElementById("selectedFileName").innerText = selectedFileName;
+    var img = document.getElementById("fileImage");
+    img.src = '/static/' + selectedFilePath;
+}
+
+function filterFiles() {
+    const showAnnotated = document.getElementById('showAnnotated').checked;
+    const select = document.getElementById('fruits');
+    
+    for (let i = 0; i < select.options.length; i++) {
+        const option = select.options[i];
+        const txtValue = option.getAttribute('data-txt');
+        
+        if (showAnnotated) {
+            if (txtValue !== 'None') {
+                option.style.display = '';
+            } else {
+                option.style.display = 'none';
+            }
+        } else {
+            option.style.display = '';
+        }
+    }
+}
+
+function updateFileLabel(files) {
+    const fileNames = Array.from(files).map(file => file.name).join(', ');
+    const label = document.querySelector('.selectFolderBtn-btn');
+    label.textContent = fileNames.length > 0 ? `Выбрано файлов: ${fileNames}` : 'Выбрать папку';
+}
+
+document.getElementById('photoForm').onsubmit = function(event) {
+    event.preventDefault();
+    const numPhotos = document.getElementById('numPhotos').value;
+    const destinationFolder = document.getElementById('destinationFolder').value;
+
+    const formData = new FormData();
+    formData.append('num_photos', numPhotos);
+    formData.append('destination_folder', destinationFolder);
+    console.log(`Количество фотографий: ${numPhotos}`);
+    console.log(`Путь к папке назначения: ${destinationFolder}`);
+
+    fetch('/copy_photos', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message || data.error);
+    })
+    .catch(error => {
+        console.error('Ошибка:', error);
+    });
+};
+
+document.querySelector('form').addEventListener('submit', function(e) {
+    const imgsz = document.getElementById('imgsz').value;
+    const epochs = document.getElementById('epochs').value;
+    const batch = document.getElementById('batch').value;
+    const savePeriod = document.getElementById('save_period').value;
+
+    if (imgsz < 1 || epochs < 1 || batch < 1 || savePeriod < 1) {
+        e.preventDefault();
+        alert('Все значения должны быть больше 0.');
+    }
+});
+
+document.getElementById('numPhotos').addEventListener('input', function() {
+    const min = parseInt(this.min);
+    const max = parseInt(this.max);
+    const value = parseInt(this.value);
+
+    if (value < min || value > max) {
+        this.setCustomValidity(`Пожалуйста, введите число от ${min} до ${max}.`);
+    } else {
+        this.setCustomValidity('');
+    }
+});
+
+function handleFlashMessages(messages) {
+    messages.forEach(function(msg) {
+        let title = msg.category.charAt(0).toUpperCase() + msg.category.slice(1); 
+        let icon = msg.category === 'success' ? 'success' : 'error'; 
+
+        swal({
+            title: title,
+            text: msg.message,
+            icon: icon,
+            button: "ОК",
+        });
+    });
+}
+
+
+function openCreateDataSet() {
+    var LoadingScreen = document.querySelector('.CreateDataSet');
+    document.querySelector('.overlayForCD').classList.add('showForCD');
+    document.querySelector('.CreateDataSet').classList.add('showForCD');
+    LoadingScreen.style.display = 'block';
+}
+   
+function closeCreateDataSet() {
+    var LoadingScreen = document.querySelector('.CreateDataSet');
+    document.querySelector('.overlayForCD').classList.remove('showForCD');
+    document.querySelector('.CreateDataSet').classList.remove('showForCD');
+    LoadingScreen.style.display = 'none';
+}
+
+function openScript() {
+    var LoadingScreen = document.querySelector('.Script');
+    document.querySelector('.overlayForScript').classList.add('showForScript');
+    document.querySelector('.Script').classList.add('showForScript');
+    LoadingScreen.style.display = 'block';
+}
+   
+function closeScript() {
+    var LoadingScreen = document.querySelector('.Script');
+    document.querySelector('.overlayForScript').classList.remove('showForScript');
+    document.querySelector('.Script').classList.remove('showForScript');
+    LoadingScreen.style.display = 'none';
+}
+
+
+const trainInput = document.getElementById('trainSize');
+    const valInput = document.getElementById('valSize');
+
+    trainInput.addEventListener('input', function() {
+        const trainValue = parseFloat(trainInput.value);
+        if (!isNaN(trainValue)) {
+            valInput.value = (1 - trainValue).toFixed(2);
+        }
+    });
+
+    valInput.addEventListener('input', function() {
+        const valValue = parseFloat(valInput.value);
+        if (!isNaN(valValue)) {
+            trainInput.value = (1 - valValue).toFixed(2);
+        }
+    });
+
+
+$(document).ready(function() {
+    $('#photoForm').on('submit', function(event) {
+        event.preventDefault(); // Prevent default form submission
+
+        $.ajax({
+            type: 'POST',
+            url: $(this).attr('action'),
+            data: $(this).serialize(),
+            success: function(response) {
+                if (response.exists) {
+                    // Display the message in an alert box
+                    alert(response.message);
+                    // Ask the user to confirm
+                    if (confirm("Продолжить?")) {
+                        // If the user confirms, send the form again
+                        $.ajax({
+                            type: 'POST',
+                            url: '/copy_photos', // Same handler
+                            data: $(this).serialize(),
+                            success: function() {
+                                window.location.href = '/'; // Redirect to the main page
+                            },
+                            error: function(err) {
+                                alert('Ошибка: ' + err.responseJSON.error);
+                            }
+                        });
+                    }
+                } else {
+                    // If the folder doesn't exist, continue with the copy operation
+                    window.location.href = '/'; // Redirect to the main page
+                }
+            },
+            error: function(err) {
+                alert('Ошибка: ' + err.responseJSON.error);
+            }
+        });
+    });
+});
+
+
+function updateFileCount() {
+    const select = document.getElementById('datasets');
+    const selectedOptions = Array.from(select.selectedOptions);
+    const count = selectedOptions.length;
+
+    console.log(`Количество выбранных файлов: ${count}`);
+}
